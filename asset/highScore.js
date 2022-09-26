3329d1b76e3538dbbcab430e87b26f3f47f3729e
Tvanmeveren <Tyvan07@yahoo.com>
2022-09-26 17:02:10 -0500

    updared the text in highScorejs

---
+++ asset/highScore.js
@@ -0,0 +1,21 @@
+//on highscoare page
+//get score history from local storage
+// loop through score history and put each game details on the page
+let list = document.getElementById("highScoreList")
+let clearButton= document.getElementById("clear")
+
+let userDataArray= JSON.parse(localStorage.getItem("userData"))
+
+for (let i = 0; i < userDataArray.length; i++) {
+    let newListItem = document.createElement("li")
+    let currentInitial = userDataArray[i].initials
+    let currentScore = userDataArray[i].finalScore
+    let currentItemContent= currentInitial +  " - " + currentScore
+    newListItem.textContent = currentItemContent
+    list.appendChild(newListItem)
+}
+
+clearButton.addEventListener("click", function(){
+    localStorage.clear()
+    location.reload()
+})
